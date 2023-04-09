26da5a3f0bd687d6f197385d3c4a7f16347cf8dd
FernandoT230 <fernando712torres@gmail.com>
2023-04-09 16:50:56 -0400

    r&m

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import Nav from './components/NAV';
-import characters, { Rick } from './data.js';
-import { useState } from 'react';
+import "./App.css";
+import Cards from "./components/Cards.jsx";
+import Nav from "./components/NAV";
+import characters, { Rick } from "./data.js";
+import { useState } from "react";
+
+const example = {
+   id: 1,
+   name: 'Rick Sanchez',
+   status: 'Alive',
+   species: 'Human',
+   gender: 'Male',
+   origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+   },
+   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+};
 
 function App() {
-   const [characters, setCharacters] = useState([])
-   
-   return (
-      <div className='App'>
-         <Nav />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => alert('Emulamos que se cierra la card')}
-         />
-      </div>
-   );
+  const [characters, setCharacters] = useState([]);
+
+  const onSearch = () => {
+   setCharacters(...characters, example)
+  }
+
+
+  return (
+    <div className="App">
+      <Nav onSearch={onSearch}/>
+      <Cards characters={characters} />
+    </div>
+  );
 }
 
 export default App;

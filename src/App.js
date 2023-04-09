4cff8bdb745c1402c93340d3a539860ec7287087
FernandoT230 <fernando712torres@gmail.com>
2023-04-09 23:01:00 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -3,32 +3,34 @@ import Cards from "./components/Cards.jsx";
 import Nav from "./components/NAV";
 import characters, { Rick } from "./data.js";
 import { useState } from "react";
-
-const example = {
-   id: 1,
-   name: 'Rick Sanchez',
-   status: 'Alive',
-   species: 'Human',
-   gender: 'Male',
-   origin: {
-      name: 'Earth (C-137)',
-      url: 'https://rickandmortyapi.com/api/location/1',
-   },
-   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-};
+import axios from "axios";
 
 function App() {
   const [characters, setCharacters] = useState([]);
 
-  const onSearch = () => {
-   setCharacters(...characters, example)
+  const onSearch = (id) => {
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
+        }
+      }
+    );
+  };
+  
+  
+  const onClose = (id) => {
+    const charactersFiltered = characters.filter(character => character.id !== Number(id))
+    setCharacters(charactersFiltered)
   }
-
+  
 
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <Cards characters={characters} />
+      <Nav onSearch={onSearch} />
+      <Cards characters={characters} onClose={onClose}/>
     </div>
   );
 }

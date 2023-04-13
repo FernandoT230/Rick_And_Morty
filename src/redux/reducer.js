5313462f3a4d036caf67354edcaba04d4d43afc8
FernandoT230 <fernando712torres@gmail.com>
2023-04-13 02:29:03 +0000

    Update 8 files

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,26 +1,47 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./action-types";
 
 const initialState = {
-  myFavorites: []
+  myFavorites: [],
+  allCharacters: [],
 };
 
-const reducer = (state = initialState, action) => {
+const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAV:
       return {
         ...state,
-        myFavorites: [...state.myFavorites, action.payload]
+        allCharacters: [...state.allCharacters, action.payload],
+        myFavorites: [...state.myFavorites, action.payload],
       };
     case REMOVE_FAV:
       return {
         ...state,
+        allCharacters: state.allCharacters.filter(
+          (character) => character.id !== action.payload
+        ),
         myFavorites: state.myFavorites.filter(
-          (character) => character.id !== parseInt(action.payload)
-        )
+          (character) => character.id !== action.payload
+        ),
+      };
+    case FILTER:
+      const filteredCharacters = state.allCharacters.filter(
+        (character) => character.gender === action.payload
+      );
+      return {
+        ...state,
+        myFavorites: filteredCharacters,
+      };
+    case ORDER:
+      const sortedCharacters = [...state.allCharacters].sort((a, b) =>
+        action.payload === "A" ? a.id - b.id : b.id - a.id
+      );
+      return {
+        ...state,
+        myFavorites: sortedCharacters,
       };
     default:
       return state;
   }
 };
 
-export default reducer;
+export default rootReducer;
